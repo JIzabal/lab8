2b1faa51377de622fabdebaf6bf12d5c26ba57c3
Jaime Izabal <jizabal@ucsd.edu>
2021-02-25 10:59:51 -0800

    Finished lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'KNsvyq6o3Yba38hr7rUQqPkAPdkcxuOR';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	center: [32.87872302170223, -117.23585382604509],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12
+	});
+
+	L.marker([32.87872302170223, -117.23585382604509]).addTo(map);
 }
